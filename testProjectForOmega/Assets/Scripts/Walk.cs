15144a33dccdcb0ffa799bc2404075b8b4b61b62
psmax <shmarov_max@mail.ru>
2016-12-04 19:07:58 +0300

    :+1:

---
+++ testProjectForOmega/Assets/Scripts/Walk.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Walk : MonoBehaviour
+{
+
+    public GameObject player;
+    public float move_speed;
+    public float maxDist;
+    public Transform coin;
+
+ //   private Vector3 look_dir;
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        transform.LookAt(coin);
+        if(Vector3.Distance(transform.position, coin.position) <= maxDist)
+        {
+            transform.position += transform.forward * move_speed * Time.deltaTime;
+        }
+    }
+
+ 
+
+}
