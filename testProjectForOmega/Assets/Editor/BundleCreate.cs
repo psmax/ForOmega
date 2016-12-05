def4284b6cc38fb93a2dd0804e56b3c06e2dd64d
psmax <shmarov_max@mail.ru>
2016-12-05 17:41:52 +0300

    withDropBox

---
+++ testProjectForOmega/Assets/Editor/BundleCreate.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+using System.Collections;
+
+public class BundleCreate
+{
+
+    [MenuItem("Simple Bundles/BuildBut")]
+    static void BuildBundles()
+    {
+        string path = EditorUtility.SaveFolderPanel("Save Bundle", "", "");  //Отображает диалоговое окно "Сохранить Папку" и возвращается
+        if (path.Length != 0)
+        {
+            BuildPipeline.BuildAssetBundles(path, BuildAssetBundleOptions.None, BuildTarget.Android);
+        }
+    }
+}
